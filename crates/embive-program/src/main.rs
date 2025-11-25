265ce539652bb6bf61c545895820de0482bb2e33
Yona Appletree <hypher@gmail.com>
2025-11-25 10:51:59 -0800

    refactor(main): simplify printing logic and remove unused functions

--- crates/embive-program/src/main.rs
+++ crates/embive-program/src/main.rs
@@ -2,62 +2,15 @@
 #![no_main]
 
 extern crate alloc;
+#[macro_use]
+extern crate embive_runtime;
 
-use alloc::{vec::Vec, string::String};
+use alloc::vec::Vec;
 use core::{panic::PanicInfo, sync::atomic::{AtomicI32, Ordering}};
 
-use embive_runtime::{ebreak, syscall, _print};
-
-/// Print macro for no_std environments
-#[macro_export]
-macro_rules! print {
-    ($($arg:tt)*) => {
-        $crate::_print(core::format_args!($($arg)*));
-    };
-}
-
-/// Println macro for no_std environments
-#[macro_export]
-macro_rules! println {
-    () => {
-        $crate::print!("\n");
-    };
-    ($($arg:tt)*) => {
-        $crate::print!("{}\n", core::format_args!($($arg)*));
-    };
-}
+use embive_runtime::{ebreak, syscall};
 
 static RESULT: AtomicI32 = AtomicI32::new(0);
-
-/// Format a number as a string (no_std compatible)
-fn format_number(mut n: i32) -> String {
-    let mut s = String::new();
-    let negative = n < 0;
-    if negative {
-        n = -n;
-    }
-    
-    if n == 0 {
-        s.push('0');
-    } else {
-        let mut digits = Vec::new();
-        while n > 0 {
-            digits.push((b'0' + (n % 10) as u8) as char);
-            n /= 10;
-        }
-        for &digit in digits.iter().rev() {
-            s.push(digit);
-        }
-    }
-    
-    if negative {
-        let mut result = String::from("-");
-        result.push_str(&s);
-        result
-    } else {
-        s
-    }
-}
 
 /// Panics will simply exit the interpreter (ebreak)
 #[panic_handler]
@@ -93,29 +46,17 @@ pub extern "Rust" fn main() {
     if let Ok(value) = result {
         RESULT.store(value, Ordering::SeqCst);
         
-        // Test heap allocation with String
-        let mut message = String::from("Sum of ");
-        // Format numbers manually since to_string requires std
-        let num1_str = format_number(numbers[0]);
-        let num2_str = format_number(numbers[1]);
-        let result_str = format_number(value);
-        message.push_str(&num1_str);
-        message.push_str(" and ");
-        message.push_str(&num2_str);
-        message.push_str(" is ");
-        message.push_str(&result_str);
-        println!("[guest] {}", message);
+        println!("[guest] Sum of {} and {} is {}", numbers[0], numbers[1], value);
         
         // Show Vec contents
-        let mut vec_str = String::from("Vec contents: [");
+        print!("[guest] Vec contents: [");
         for (i, &num) in numbers.iter().enumerate() {
             if i > 0 {
-                vec_str.push_str(", ");
+                print!(", ");
             }
-            vec_str.push_str(&format_number(num));
+            print!("{}", num);
         }
-        vec_str.push(']');
-        println!("[guest] {}", vec_str);
+        println!("]");
     }
     
     // Test multiple allocations
--- crates/embive-runtime/src/print.rs
+++ crates/embive-runtime/src/print.rs
@@ -39,3 +39,23 @@ pub fn _print(args: fmt::Arguments) {
     }
 }
 
+/// Print macro for no_std environments
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => {
+        $crate::_print(core::format_args!($($arg)*));
+    };
+}
+
+/// Println macro for no_std environments
+#[macro_export]
+macro_rules! println {
+    () => {
+        $crate::print!("\n");
+    };
+    ($($arg:tt)*) => {
+        $crate::print!($($arg)*);
+        $crate::print!("\n");
+    };
+}
+
